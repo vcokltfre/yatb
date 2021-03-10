e0435ff61f24d882fb785acc3a264ca37175475a
vcokltfre <vcokltfre@gmail.com>
2021-03-10 03:47:56 +0000

    feat: add custom context

---
+++ src/internal/context.py
@@ -0,0 +1,7 @@
+from discord.ext.commands import Context as _BaseContext
+
+
+class Context(_BaseContext):
+    """A Custom Context for extra functionality."""
+
+    pass # Use to extend context functionality
