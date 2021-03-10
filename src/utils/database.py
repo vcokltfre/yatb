bf886905b95700ae4aaf218d00e8811c5def9c2d
vcokltfre <vcokltfre@gmail.com>
2021-03-10 03:46:39 +0000

    feat: add database util

---
+++ src/utils/database.py
@@ -0,0 +1,30 @@
+from asyncpg import create_pool
+from os import getenv
+
+
+class Database:
+    """A database interface for the bot to connect to Postgres."""
+
+    def __init__(self):
+        self.guilds = {}
+
+    async def setup(self):
+        self.pool = await create_pool(
+            host=getenv("DB_HOST", "127.0.0.1"),
+            port=getenv("DB_PORT", 5432),
+            database=getenv("DB_DATABASE"),
+            user=getenv("DB_USER", "root"),
+            password=getenv("DB_PASS", "password"),
+        )
+
+    async def execute(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            await conn.execute(query, *args)
+
+    async def fetchrow(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+
+    async def fetch(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
