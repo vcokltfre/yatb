f547b7580c05a713a656124c77642ca9765abc11
vcokltfre <vcokltfre@gmail.com>
2021-03-10 03:55:10 +0000

    feat: add logging to database

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,6 +1,8 @@
 from asyncpg import create_pool
 from os import getenv
 
+from loguru import logger
+
 
 class Database:
     """A database interface for the bot to connect to Postgres."""
@@ -9,6 +11,7 @@ class Database:
         self.guilds = {}
 
     async def setup(self):
+        logger.info("Setting up database...")
         self.pool = await create_pool(
             host=getenv("DB_HOST", "127.0.0.1"),
             port=getenv("DB_PORT", 5432),
@@ -16,6 +19,7 @@ class Database:
             user=getenv("DB_USER", "root"),
             password=getenv("DB_PASS", "password"),
         )
+        logger.info("Database setup complete.")
 
     async def execute(self, query: str, *args):
         async with self.pool.acquire() as conn:
