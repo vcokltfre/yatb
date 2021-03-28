a54998a3b2c95e499a1d16b7f2c5b1d8565bc8c7
vcokltfre <vcokltfre@gmail.com>
2021-03-28 13:51:23 +0100

    feat: add automigration

---
+++ src/data/0001-init.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS Migrations (
+    id          SERIAL PRIMARY KEY
+);
--- src/utils/database.py
+++ src/utils/database.py
@@ -1,5 +1,6 @@
 from asyncpg import create_pool
-from os import getenv
+from traceback import format_exc
+from os import getenv, walk
 
 from loguru import logger
 
@@ -20,6 +21,58 @@ class Database:
             password=getenv("DB_PASS", "password"),
         )
         logger.info("Database setup complete.")
+
+        await self.automigrate()
+
+    async def automigrate(self):
+        allow = getenv("AUTOMIGRATE", "false")
+
+        if allow != "true":
+            logger.info("Automigrating is disabled, skipping migration attempt.")
+            return
+
+        try:
+            migration = await self.fetchrow("SELECT id FROM Migrations ORDER BY id DESC LIMIT 1;")
+        except Exception as e:
+            print(e)
+            migration = None
+
+        migration = migration["id"] if migration else 0
+
+        fs = []
+
+        for root, dirs, files in walk("./src/data/"):
+            for file in files:
+                mnum = int(file[0:4])
+                fs.append((mnum, file))
+
+        fs.sort()
+        fs = [f for f in fs if f[0] > migration]
+
+        if not fs:
+            return
+
+        logger.info("Running automigrate...")
+
+        for file in fs:
+            res = await self.run_migration(file[1], file[0])
+            if not res:
+                break
+
+        logger.info("Finished automigrate.")
+
+    async def run_migration(self, filename: str, num: int):
+        logger.info(f"Running migration {filename}...")
+        try:
+            with open(f"./src/data/{filename}") as f:
+                await self.execute(f.read())
+            await self.execute("INSERT INTO Migrations VALUES ($1);", num)
+        except Exception as e:
+            logger.error(f"Failed to run migration {filename}: {format_exc()}")
+            return False
+        else:
+            logger.info(f"Successfully ran migration {filename}.")
+            return True
 
     async def execute(self, query: str, *args):
         async with self.pool.acquire() as conn:
